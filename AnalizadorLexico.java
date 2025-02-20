d344d5c89e6bef16c800351c609955ad467e2725
Marcos Alemany <marcos.alemany.m@gmail.com>
2025-02-20 12:50:47 +0100

    update

--- AnalizadorLexico.java
+++ AnalizadorLexico.java
@@ -1,9 +1,12 @@
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class AnalizadorLexico
 {
     private RandomAccessFile raf;
+    private int fila = 1;
+    private int columna = 1;
 
     public AnalizadorLexico(RandomAccessFile entrada)
     {
@@ -12,21 +15,89 @@ public class AnalizadorLexico
 
     public Token siguienteToken()
     {
-        Token token = new Token();
         char caracter = this.leer();
+        int estado = 0;
 
-        return token;
+        do
+        {
+            int nuevo = delta(estado, caracter);
+            if(nuevo == -2)
+            {
+                System.out.println("Error lexico: fin de fichero inesperado");
+                System.exit(-1);
+            }
+            else if(nuevo == -1)
+            {
+                System.out.println("Error lexico (" + this.fila + "," + this.columna
+                        + "): caracter '" + caracter + "' incorrecto");
+
+                System.exit(-1);
+            }
+            else if(this.esFinal(estado))
+            {
+                devolverCaracteres(estado);
+                return new Token();
+            }
+            else
+            {
+                estado = nuevo;
+                caracter = this.leer();
+                this.columna++;
+
+                if(caracter == '\n')
+                {
+                    this.fila++;
+                    this.columna = 1;
+                    caracter = this.leer();
+                }
+            }
+        } while (true);
+
+
     }
 
     private char leer()
     {
         try{
-            return this.raf.readChar();
+            return (char)this.raf.read();
+        }
+        catch (EOFException e)
+        {
+            return Token.EOF;
         }
         catch(IOException e)
         {
             return '\0';
         }
 
+    }
+
+    private int delta(int estado, int c)
+    {
+        switch(estado)
+        {
+            default: return 0;
+        }
+    }
+
+    private boolean esFinal(int estado)
+    {
+        return true;
+    }
+
+    private void devolverCaracteres(int estado)
+    {
+        try
+        {
+            switch(estado)
+            {
+                default:
+                this.raf.seek(this.raf.getFilePointer() - 1);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
