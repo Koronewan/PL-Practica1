d0ec0c2f0fc9a891a1db304e60ae0ed081d53f21
Marcos Alemany <108985619+ILoveWaifus@users.noreply.github.com>
2025-02-20 11:48:56 +0100

    Update AnalizadorLexico.java

--- AnalizadorLexico.java
+++ AnalizadorLexico.java
@@ -1,2 +1,32 @@
-public class AnalizadorLexico {
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class AnalizadorLexico
+{
+    private RandomAccessFile raf;
+
+    public AnalizadorLexico(RandomAccessFile entrada)
+    {
+        this.raf = entrada;
+    }
+
+    public Token siguienteToken()
+    {
+        Token token = new Token();
+        char caracter = this.leer();
+
+        return token;
+    }
+
+    private char leer()
+    {
+        try{
+            return this.raf.readChar();
+        }
+        catch(IOException e)
+        {
+            return '\0';
+        }
+
+    }
 }
